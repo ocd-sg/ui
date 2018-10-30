853fd1e741724d63b11ad9c23ee289c39c48de8d
Yin Shanyang <me@yinshanyang.com>
2018-10-30 19:44:27 +0800

    refactor: switch to `mini-css-extract`

--- modules/redux-module/config/webpack.config.build-app.js
+++ modules/redux-module/config/webpack.config.build-app.js
@@ -2,7 +2,7 @@ const paths = require("../../../utils/paths");
 const webpack = require("webpack");
 const baseConfig = require("./webpack.config.base");
 
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const MiniCssExtractTextPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const { TITLE = "Module" } = process.env;
@@ -16,11 +16,38 @@ const config = {
     filename: "bundle.min.js",
     globalObject: "this"
   },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        include: paths.projectSourcePath,
+        use: [
+          {
+            loader: require.resolve("ts-loader"),
+            options: {
+              transpileOnly: true
+            }
+          }
+        ]
+      },
+      {
+        test: /(\.css)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: require.resolve("css-loader")
+          }
+        ]
+      }
+    ]
+  },
   plugins: [
     ...baseConfig.plugins,
-    new ExtractTextPlugin({
+    new MiniCssExtractPlugin({
       filename: "styles.css",
-      allChunks: true
+      chunks: "all"
     }),
     new HtmlWebpackPlugin({
       title: TITLE
