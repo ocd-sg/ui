96a00865e0861f5b83db78c9d550cd46f23584ac
Yin Shanyang <me@yinshanyang.com>
2018-11-07 16:41:49 +0800

    refactor: tidy up `react-component` tasks

--- modules/react-component/tasks/buildApp.js
+++ modules/react-component/tasks/buildApp.js
@@ -1,18 +1,13 @@
-const cp = require('child_process')
 const path = require('path')
 const paths = require('../../../utils/paths')
+const storybook = require('@storybook/react/standalone')
 
 const buildApp = () => {
-  const storybook = require.resolve('@storybook/react/bin/build')
-  const config = path.resolve(__dirname, '../app')
-  const out = paths.projectBuildAppPath
-  const child = cp.spawn(
-    storybook,
-    `--config-dir ${config} -o ${out}`.split(' ')
-  )
-
-  child.stdout.pipe(process.stdout)
-  child.stderr.pipe(process.stderr)
+  storybook({
+    mode: 'static',
+    configDir: path.resolve(__dirname, '../app'),
+    outputDir: paths.projectBuildAppPath
+  })
 }
 
 module.exports = buildApp
