bb46c98650cc9d10a259ac1613f3ba997c150329
Yin Shanyang <me@yinshanyang.com>
2018-11-10 17:53:01 +0800

    feature: addons for storybook

--- modules/redux-module/storybook/config.js
+++ modules/redux-module/storybook/config.js
@@ -1,8 +1,21 @@
 const req = require.context('project', true, /story\.tsx$/)
-const { configure } = require('@storybook/react')
+const { configure, addDecorator } = require('@storybook/react')
+const { withInfo } = require('@storybook/addon-info')
 
-const loadStories = () => {
-  req.keys().forEach(req)
-}
-
+addDecorator(withInfo({
+  header: false,
+  inline: false,
+  propTables: null,
+  styles: (stylesheets) => ({
+    ...stylesheets,
+    children: {
+      ...stylesheets.children,
+      height: '100vh'
+    }
+  })
+}))
 configure(loadStories, module)
+
+function loadStories () {
+  return req.keys().forEach(req)
+}
